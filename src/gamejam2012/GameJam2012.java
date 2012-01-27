c3db118f4f3ab192a1bb248f91edf4c0ce684018
Scott Adams <Scott Adams@192.168.1.123>
2012-01-26 22:40:42 -0500

    First Commit

---
+++ src/gamejam2012/GameJam2012.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamejam2012;
+
+/**
+ *
+ * @author Scott Adams
+ */
+public class GameJam2012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
